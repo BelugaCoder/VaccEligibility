3af8f44efec39bd4aa1965bedea900fcbc689982
BeluCoder <98012766+BelugaCoder@users.noreply.github.com>
2022-01-28 22:17:56 +0530

    Add files via upload

---
+++ main.py
@@ -0,0 +1,12 @@
+# This Programs let you know your eligibility for Vaccination in INDIA.
+
+age = int(input('Enter Your Year of Birth -> '))
+
+if age < 1922:
+    print('Are u still alive ???')
+
+if age > 2008:
+    print('You are not eligible for Vaccination')
+
+else:
+    print('You are eligible for Vaccination')
